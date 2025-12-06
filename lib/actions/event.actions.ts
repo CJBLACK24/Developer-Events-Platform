351444bd271723088314f5216abc54164c91335b
DUQUE CHRISTIAN JOHN C <duquechristianjohncalderon@gmail.com>
2025-12-06 18:16:01 +0800

    feat: Add event listing and detail pages, along with event data model and actions.

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,15 +1,44 @@
-'use server';
+"use server";
 
-import Event from '@/database/event.model';
+import Event, { IEvent } from "@/database/event.model";
 import connectDB from "@/lib/mongodb";
 
-export const getSimilarEventsBySlug = async (slug: string) => {
-    try {
-        await connectDB();
-        const event = await Event.findOne({ slug });
+export const getSimilarEventsBySlug = async (
+  slug: string
+): Promise<IEvent[]> => {
+  try {
+    await connectDB();
+    const event = await Event.findOne({ slug });
 
-        return await Event.find({ _id: { $ne: event._id }, tags: { $in: event.tags } }).lean();
-    } catch {
-        return [];
-    }
-}
+    const similarEvents = await Event.find({
+      _id: { $ne: event._id },
+      tags: { $in: event.tags },
+    }).lean();
+    return JSON.parse(JSON.stringify(similarEvents));
+  } catch {
+    return [];
+  }
+};
+
+export const getAllEvents = async () => {
+  try {
+    await connectDB();
+    const events = await Event.find().sort({ createdAt: -1 });
+
+    return JSON.parse(JSON.stringify(events));
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+};
+
+export const getEventBySlug = async (slug: string) => {
+  try {
+    await connectDB();
+    const event = await Event.findOne({ slug });
+    return JSON.parse(JSON.stringify(event));
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
